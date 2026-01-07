a99881039619e5a5ddbc74a82c451d63e61a3bc3
CarlosCuadrado-24 <carloscuadradot24@gmail.com>
2026-01-07 14:18:31 -0500

    feat(commands): GREEN TEST - implement PrepareColdDishCommand

---
+++ src/main/java/com/foodtech/kitchen/domain/commands/PrepareColdDishCommand.java
@@ -0,0 +1,33 @@
+package com.foodtech.kitchen.domain.commands;
+
+import com.foodtech.kitchen.domain.model.Product;
+import com.foodtech.kitchen.domain.model.Station;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrepareColdDishCommand implements Command {
+    private final List<Product> products;
+
+    public PrepareColdDishCommand(List<Product> products) {
+        this.products = new ArrayList<>(products);
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("Preparing cold dishes at COLD_KITCHEN:");
+        for (Product product : products) {
+            System.out.println("  - " + product.getName());
+        }
+    }
+
+    @Override
+    public Station getStation() {
+        return Station.COLD_KITCHEN;
+    }
+
+    @Override
+    public List<Product> getProducts() {
+        return new ArrayList<>(products);
+    }
+}
