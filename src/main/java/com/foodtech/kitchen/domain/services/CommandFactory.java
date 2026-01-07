13ca5a37c2ec67e26ed79d6647dcf3aa45eb1303
CarlosCuadrado-24 <carloscuadradot24@gmail.com>
2026-01-07 14:31:43 -0500

    feat(services): GREEN TEST - implement CommandFactory

---
+++ src/main/java/com/foodtech/kitchen/domain/services/CommandFactory.java
@@ -0,0 +1,18 @@
+package com.foodtech.kitchen.domain.services;
+
+import com.foodtech.kitchen.domain.commands.*;
+import com.foodtech.kitchen.domain.model.Product;
+import com.foodtech.kitchen.domain.model.Station;
+
+import java.util.List;
+
+public class CommandFactory {
+
+    public Command createCommand(Station station, List<Product> products) {
+        return switch (station) {
+            case BAR -> new PrepareDrinkCommand(products);
+            case HOT_KITCHEN -> new PrepareHotDishCommand(products);
+            case COLD_KITCHEN -> new PrepareColdDishCommand(products);
+        };
+    }
+}
