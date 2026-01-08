249763114842e28c52e933dcf805c392a5e3e6a9
CarlosCuadrado-24 <carloscuadradot24@gmail.com>
2026-01-07 19:36:52 -0500

    feat(execution): GREEN TEST - implement SyncCommandExecutor, Add CommandExecutor port interface, Implement synchronous command execution, Execute commands one by one, GREEN phase: SyncCommandExecutor tests pass

--- src/main/java/com/foodtech/kitchen/application/ports/out/CommandExecutor.java
+++ src/main/java/com/foodtech/kitchen/application/ports/out/CommandExecutor.java
@@ -1,5 +1,10 @@
 package com.foodtech.kitchen.application.ports.out;
 
-public class CommandExecutor {
-    
+import com.foodtech.kitchen.domain.commands.Command;
+
+import java.util.List;
+
+public interface CommandExecutor {
+    void execute(Command command);
+    void executeAll(List<Command> commands);
 }
--- src/main/java/com/foodtech/kitchen/infrastructure/execution/SyncCommandExecutor.java
+++ src/main/java/com/foodtech/kitchen/infrastructure/execution/SyncCommandExecutor.java
@@ -1,5 +1,21 @@
 package com.foodtech.kitchen.infrastructure.execution;
 
-public class SyncCommandExecutor {
-    
+import com.foodtech.kitchen.application.ports.out.CommandExecutor;
+import com.foodtech.kitchen.domain.commands.Command;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SyncCommandExecutor implements CommandExecutor {
+
+    @Override
+    public void execute(Command command) {
+        command.execute();
+    }
+
+    @Override
+    public void executeAll(List<Command> commands) {
+        commands.forEach(this::execute);
+    }
 }
